6607c86c3256a70a318575f7577a54618ad03c64
Danny Angelo Carminati Grein <fungos@gmail.com>
2018-12-13 18:47:37 -0500

    Add a new target to auto-format to auto-format the code
    This will try to download clang-format-7 from another repository that
    we'll setup.
    Cleanup unused SDL2_image and SDL2_mixer cmake files.

---
+++ CMake/ClangFormat.cmake
@@ -0,0 +1,53 @@
+# From Yuzu: https://github.com/yuzu-emu/yuzu/blob/master/CMakeLists.txt
+#
+# Setup a custom clang-format target (if clang-format can be found) that will run
+# against all the src files. This should be used before making a pull request.
+# =======================================================================
+
+set(CLANG_FORMAT_POSTFIX "-7")
+find_program(CLANG_FORMAT
+    NAMES
+      clang-format${CLANG_FORMAT_POSTFIX}
+      clang-format
+    PATHS
+      /usr/bin
+      ${PROJECT_BINARY_DIR}/Dependencies
+      $ENV{PATH}
+    )
+if (NOT CLANG_FORMAT)
+    if (WIN32)
+        message(STATUS "Clang format not found! Downloading...")
+        set(CLANG_FORMAT "${PROJECT_BINARY_DIR}/Dependencies/clang-format${CLANG_FORMAT_POSTFIX}.exe")
+        file(DOWNLOAD
+            https://github.com/OrionUO/dep-build-win/raw/master/clang-format${CLANG_FORMAT_POSTFIX}.exe
+            "${CLANG_FORMAT}" SHOW_PROGRESS
+            STATUS DOWNLOAD_SUCCESS)
+        if (NOT DOWNLOAD_SUCCESS EQUAL 0)
+            message(WARNING "Could not download clang format! Disabling the clang format target")
+            file(REMOVE ${CLANG_FORMAT})
+            unset(CLANG_FORMAT)
+        endif()
+    else()
+        message(WARNING "Clang format not found! Disabling the clang format target")
+    endif()
+endif()
+
+if (CLANG_FORMAT)
+    set(SRCS ${PROJECT_SOURCE_DIR}/OrionUO)
+    set(CCOMMENT "Running clang format against all the .h and .cpp files in OrionUO/")
+    if (WIN32)
+        add_custom_target(clang-format
+            COMMAND powershell.exe -Command "Get-ChildItem ${SRCS}/* -Include *.cpp,*.h -Recurse | Foreach {${CLANG_FORMAT} -i $_.fullname}"
+            COMMENT ${CCOMMENT})
+    elseif(MINGW)
+        add_custom_target(clang-format
+            COMMAND find `cygpath -u ${SRCS}` -iname *.h -o -iname *.cpp | xargs `cygpath -u ${CLANG_FORMAT}` -i
+            COMMENT ${CCOMMENT})
+    else()
+        add_custom_target(clang-format
+            COMMAND find ${SRCS} -iname *.h -o -iname *.cpp | xargs ${CLANG_FORMAT} -i
+            COMMENT ${CCOMMENT})
+    endif()
+    unset(SRCS)
+    unset(CCOMMENT)
+endif()
