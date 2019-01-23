31cf0f11274027fb7893c0c3ae6f06fbedcfa638
Danny Angelo Carminati Grein <fungos@gmail.com>
2019-01-22 19:18:33 -0500

    Format

--- OrionUO/Logging.h
+++ OrionUO/Logging.h
@@ -11,15 +11,15 @@ void LogDump(FILE *fp, uint8_t *buf, int size);
 
 #if LOG_LEVEL == 1
 #define INITLOGGER(path)
-#define LOG(...)            fprintf(stdout, " LOG: " __VA_ARGS__)
+#define LOG(...) fprintf(stdout, " LOG: " __VA_ARGS__)
 #define LOG_DUMP(...)      //LogDump(stdout, __VA_ARGS__)
 #define SAFE_LOG_DUMP(...) //LogDump(stdout, __VA_ARGS__)
 #elif LOG_LEVEL == 2
-#define INITLOGGER(path)    g_Logger.Init(path);
-#define LOG(...)            g_Logger.Print(__VA_ARGS__) //fprintf(stdout, " LOG: " __VA_ARGS__)
-#define LOG_DUMP(...)      //LogDump(stdout, __VA_ARGS__)
-#define SAFE_LOG_DUMP(...) //LogDump(stdout, __VA_ARGS__)
-#else                      //LOG_LEVEL == 0
+#define INITLOGGER(path) g_Logger.Init(path);
+#define LOG(...) g_Logger.Print(__VA_ARGS__) //fprintf(stdout, " LOG: " __VA_ARGS__)
+#define LOG_DUMP(...)                        //LogDump(stdout, __VA_ARGS__)
+#define SAFE_LOG_DUMP(...)                   //LogDump(stdout, __VA_ARGS__)
+#else                                        //LOG_LEVEL == 0
 #define INITLOGGER(path)
 #define LOG(...)
 #define LOG_DUMP(...)
