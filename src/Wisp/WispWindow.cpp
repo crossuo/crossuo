4fb1c338e42c825845f7de70fbabdf4acb5869eb
Danny Angelo Carminati Grein <fungos@gmail.com>
2019-02-02 00:23:08 -0500

    Add icon to the client, minor cmake fix (#24)
    * Add icon to the client, minor cmake fix

--- src/Wisp/WispWindow.cpp
+++ src/Wisp/WispWindow.cpp
@@ -3,8 +3,7 @@
 #include "WispWindow.h"
 #include <SDL.h>
 #include <SDL_syswm.h>
-#include <SDL_timer.h>
-#include <SDL_rect.h>
+#include "../Icon.h"
 #include "../Point.h"
 #include "../GameWindow.h"
 #include "../Managers/PluginManager.h"
@@ -150,6 +149,33 @@ bool CWindow::Create(
         SDL_LogError(SDL_LOG_CATEGORY_VIDEO, "Coult not create window: %s\n", SDL_GetError());
         return false;
     }
+
+    uint32_t rmask, gmask, bmask, amask;
+#if SDL_BYTEORDER == SDL_BIG_ENDIAN
+    int shift = (g_icon.bytes_per_pixel == 3) ? 8 : 0;
+    rmask = 0xff000000 >> shift;
+    gmask = 0x00ff0000 >> shift;
+    bmask = 0x0000ff00 >> shift;
+    amask = 0x000000ff >> shift;
+#else
+    rmask = 0x000000ff;
+    gmask = 0x0000ff00;
+    bmask = 0x00ff0000;
+    amask = (g_icon.bytes_per_pixel == 3) ? 0 : 0xff000000;
+#endif
+
+    SDL_Surface *icon = SDL_CreateRGBSurfaceFrom(
+        (void *)g_icon.pixel_data,
+        g_icon.width,
+        g_icon.height,
+        g_icon.bytes_per_pixel * 8,
+        g_icon.bytes_per_pixel * g_icon.width,
+        rmask,
+        gmask,
+        bmask,
+        amask);
+    SDL_SetWindowIcon(m_window, icon);
+    SDL_FreeSurface(icon);
 
     SDL_SysWMinfo info;
     SDL_VERSION(&info.version);
