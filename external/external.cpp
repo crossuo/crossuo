2da6dc3f63310e81c30d86fba1117a6e1386620a
Danny Angelo Carminati Grein <fungos@gmail.com>
2020-05-21 21:29:53 -0400

    deps: add base64 and original compatible zlib (amalgamated)

--- external/external.cpp
+++ external/external.cpp
@@ -23,3 +23,7 @@
 // inih - https://github.com/benhoyt/inih/tree/0a87bf16693e35ed172ce8738404d9acd9545a5a/
 #define INIH_IMPLEMENTATION
 #include "inih.h"
+
+// cbase64 - https://github.com/SizzlingCalamari/cbase64/tree/a353cab21c021aa4b16135baf909452b3ddc1173
+#define CBASE64_IMPLEMENTATION
+#include "cbase64.h"
