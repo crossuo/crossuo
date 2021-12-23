18cabb7878b8599df83b2c8a895b78577429ef43
Danny Angelo Carminati Grein <fungos@gmail.com>
2021-12-23 10:17:15 -0500

    CI: fix win64 package name

--- .github/package.cmd
+++ .github/package.cmd
@@ -5,4 +5,4 @@ echo Building %GITHUB_REF_NAME% package
 md crossuo-win64-%GITHUB_REF_NAME%
 copy build\src\Release\crossuo.exe crossuo-win64-%GITHUB_REF_NAME%\crossuo.exe
 copy build\tools\xuoi\Release\xuolauncher.exe crossuo-win64-%GITHUB_REF_NAME%\xuolauncher.exe
-7z a crossuo-win64-%GITHUB_REF_NAME%.zip crossuo-win64-%GITHUB_REF_NAME%\
+7z a crossuo-win64-%GITHUB_REF_NAME%.zip crossuo-win64-v%GITHUB_REF_NAME%\
